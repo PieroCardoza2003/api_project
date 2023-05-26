00c14731a0c99c961da0b1d1d72c0c4997455555
Piero Cardoza <82919446+PieroCardoza2003@users.noreply.github.com>
2023-05-26 04:16:26 -0500

    Update index.controller.js

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,13 +4,13 @@ export const ping = async (req, res) => {
     //const [result] = await pool.query('SELECT "SI HAY CONEXION CON LA BD" AS result')
 
     try{
-        const consulta3 = `CREATE PROCEDURE sp_cambiar_pass(
-	IN _codigo varchar(7),
-    IN _oldpasswrd varchar(80),
-    IN _newpasswrd varchar(80)
+        const consulta3 = `CREATE PROCEDURE sp_iniciar_sesion(
+	IN _usuario varchar(80),
+    IN _passwrd varchar(80)
 )
 BEGIN
-	DECLARE existe INT DEFAULT 0;
+	DECLARE nivel varchar(1);
+    DECLARE fallo varchar(7) DEFAULT 'x';
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
         ROLLBACK;
@@ -18,13 +18,30 @@ BEGIN
     END;
     
     START TRANSACTION;
-    SELECT COUNT(*) INTO existe FROM DATOS WHERE id_user = _codigo and passwrd = _oldpasswrd;
-		IF existe > 0 THEN
-			UPDATE DATOS SET passwrd = _newpasswrd WHERE id_user = _codigo;
-            SELECT '0' as fallo;
+
+    SELECT COALESCE(RIGHT(_usuario, 1), '6') INTO nivel;
+
+		IF nivel = '0' THEN
+			SELECT d.id_user INTO fallo FROM DATOS d 
+				INNER JOIN ADMINISTRADOR a ON d.id_user = a.id_admin 
+			WHERE d.usuario = _usuario AND d.passwrd = _passwrd AND a.estado = 'A';
+		ELSEIF nivel = '1' THEN
+			SELECT d.id_user INTO fallo FROM DATOS d 
+				INNER JOIN DIGITADOR di ON d.id_user = di.id_digitador 
+			WHERE d.usuario = _usuario AND d.passwrd = _passwrd AND di.estado = 'A';
+        ELSEIF nivel = '2' THEN
+			SELECT d.id_user INTO fallo FROM DATOS d 
+				INNER JOIN RUNNER r ON d.id_user = r.id_runner 
+			WHERE d.usuario = _usuario AND d.passwrd = _passwrd AND r.estado = 'A';
 		ELSE
 			SELECT '1' as fallo;
         END IF;
+
+        IF LENGTH(fallo) = 7 THEN
+			SELECT fallo;
+		ELSE
+			SELECT '1' fallo;
+		END IF;
     COMMIT;
 END`; 
         
