e3362e4fe39636379f6eb59140fe55cfc98ced9c
Piero Cardoza <82919446+PieroCardoza2003@users.noreply.github.com>
2023-05-26 04:08:27 -0500

    Update index.controller.js

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,19 +4,19 @@ export const ping = async (req, res) => {
     //const [result] = await pool.query('SELECT "SI HAY CONEXION CON LA BD" AS result')
 
     try{
-        const consulta3 = `CREATE FUNCTION fn_generar_codigo() RETURNS varchar(7)
+        const consulta3 = `CREATE FUNCTION fn_generar_usuario(nombre varchar(80), apellido varchar(80), nivel int) RETURNS varchar(80)
     READS SQL DATA
     DETERMINISTIC
 BEGIN
-    DECLARE codigo VARCHAR(7);
-    DECLARE codigo_existe INT;
+    DECLARE _usuario VARCHAR(80);
+    DECLARE existe INT;
 
     REPEAT
-        SET codigo = LPAD(FLOOR(RAND() * 10000000), 7, '0');
-        SELECT COUNT(*) INTO codigo_existe FROM DATOS WHERE id_user = codigo;
-    UNTIL codigo_existe = 0 END REPEAT;
+        SET _usuario = LOWER(CONCAT(TRIM(SUBSTRING_INDEX(nombre, ' ', 1)) ,'.', TRIM(SUBSTRING_INDEX(apellido, ' ', 1)) , LPAD(FLOOR(RAND() * 100), 2, '0') , nivel));
+        SELECT COUNT(*) INTO existe FROM DATOS WHERE usuario = _usuario;
+    UNTIL existe = 0 END REPEAT;
 
-	RETURN codigo;
+	RETURN _usuario;
 END`; 
         
     
