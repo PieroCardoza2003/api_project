e22e2262db668051173437009266ca43faeeb53d
Piero Cardoza <82919446+PieroCardoza2003@users.noreply.github.com>
2023-05-26 03:55:45 -0500

    Update index.controller.js

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,7 +4,14 @@ export const ping = async (req, res) => {
     //const [result] = await pool.query('SELECT "SI HAY CONEXION CON LA BD" AS result')
 
     try{
-        const consulta3 = `drop table SUCURSAL`; 
+        const consulta3 = `CREATE TABLE RUNNER(
+	id_runner varchar(7) primary key,
+	nombre varchar(80) not null,
+	apellidos varchar(80) not null,
+	estado char(1) not null,
+    CONSTRAINT ck_estado CHECK (estado IN ('A', 'I')),
+    CONSTRAINT fk_datos_runner FOREIGN KEY (id_runner) REFERENCES DATOS(id_user) ON DELETE CASCADE
+)`; 
         
     
         const [res3] = await pool.query(consulta3);
