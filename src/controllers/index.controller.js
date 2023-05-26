5d712ffa71fac2dec012dce83fd55d4b930019cd
PieroCardoza2003 <pierocardozazapata@gmail.com>
2023-05-26 03:36:55 -0500

    up8

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -5,19 +5,7 @@ export const ping = async (req, res) => {
 
     const er = '';
     try{
-        const consulta = `CREATE TABLE DATOS(
-            id_user varchar(7) primary key,
-            dni char(8) not null,
-            telefono char(9) null,
-            email varchar(128) not null,
-            usuario varchar(80) not null,
-            passwrd varchar(80) not null,
-            CONSTRAINT uq_dni UNIQUE (dni),
-            CONSTRAINT uq_usuario UNIQUE (usuario),
-            CONSTRAINT ck_dni CHECK (dni REGEXP '^[0-9]{8}$'),
-            CONSTRAINT ck_telefono CHECK (telefono REGEXP '^[0-9]{9}$'),
-            CONSTRAINT ck_email CHECK (email REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$')
-        );`; 
+        const consulta = `show tables;`; 
     
      
         const [result] = await pool.query(consulta);
