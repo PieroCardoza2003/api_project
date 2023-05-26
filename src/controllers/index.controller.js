74e7b3fb0d19b441732c9d1c7cb07b1562ce8fc8
PieroCardoza2003 <pierocardozazapata@gmail.com>
2023-05-26 02:47:46 -0500

    go2 update

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,6 +1,8 @@
 import {pool} from '../db.js'
 
 export const ping = async (req, res) => {
-    const [result] = await pool.query('SELECT "SI HAY CONEXION CON LA BD" AS result')
+    //const [result] = await pool.query('SELECT "SI HAY CONEXION CON LA BD" AS result')
+
+    const [result] = await pool.query('show tables')
     res.json(result[0])
 }
