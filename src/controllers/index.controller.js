ed8ea0fc53ba50473779745bea385d802b0f52d6
Piero Cardoza <82919446+PieroCardoza2003@users.noreply.github.com>
2023-05-26 04:13:54 -0500

    Update index.controller.js

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,18 +4,11 @@ export const ping = async (req, res) => {
     //const [result] = await pool.query('SELECT "SI HAY CONEXION CON LA BD" AS result')
 
     try{
-        const consulta3 = `CREATE PROCEDURE sp_actualizar(
-	IN _codigo varchar(7),
-	IN _nombre varchar(80),
-	IN _apellidos varchar(80),
-	IN _dni char(8),
-	IN _telefono char(9),
-	IN _email varchar(128),
-	IN _usuario varchar(80),
-	IN _passwrd varchar(80),
-	IN _nivel int
+        const consulta3 = `CREATE PROCEDURE sp_eliminar(
+	IN _codigo varchar(7)
 )
 BEGIN
+	DECLARE existe INT DEFAULT 0;
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
         ROLLBACK;
@@ -23,21 +16,15 @@ BEGIN
     END;
 
     START TRANSACTION;
-    
-    UPDATE DATOS SET dni = _dni , telefono = _telefono, email = LOWER(_email), usuario = LOWER(_usuario), passwrd = _passwrd WHERE id_user = _codigo;
-    
-		CASE _nivel -- | 0: admin | 1: digitador | 2: runner |
-			WHEN 0 THEN
-				UPDATE ADMINISTRADOR SET nombre = UPPER(_nombre) , apellidos = UPPER(_apellidos) WHERE id_admin = _codigo;
-			WHEN 1 THEN
-				UPDATE DIGITADOR SET nombre = UPPER(_nombre) , apellidos = UPPER(_apellidos) WHERE id_digitador = _codigo;
-			WHEN 2 THEN
-				UPDATE RUNNER SET nombre = UPPER(_nombre) , apellidos = UPPER(_apellidos) WHERE id_runner = _codigo;
-			ELSE
-				ROLLBACK;
-		END CASE;
+		SELECT COUNT(*) INTO existe FROM DATOS WHERE id_user = _codigo;
+        
+        IF existe > 0 THEN
+			DELETE FROM DATOS WHERE id_user = _codigo;
+            SELECT '0' as fallo;
+		ELSE
+			SELECT '1' as fallo;
+		END IF;
     COMMIT;
-    SELECT '0' as fallo;
 END`; 
         
     
