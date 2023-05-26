faa9f298d3522b90fcb457f15d50654f9c16e006
Piero Cardoza <82919446+PieroCardoza2003@users.noreply.github.com>
2023-05-26 03:39:08 -0500

    Update index.controller.js

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -3,15 +3,11 @@ import {pool} from '../db.js'
 export const ping = async (req, res) => {
     //const [result] = await pool.query('SELECT "SI HAY CONEXION CON LA BD" AS result')
 
-    const er = '';
     try{
         const consulta = `show tables;`; 
     
      
         const [result] = await pool.query(consulta);
-    
-        er = result;
-
         res.json(result);
 
         }
