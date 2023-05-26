45ad450b135d85783e02ad8667f9f699badc1e87
Piero Cardoza <82919446+PieroCardoza2003@users.noreply.github.com>
2023-05-26 03:44:52 -0500

    Update index.controller.js

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,9 +4,7 @@ export const ping = async (req, res) => {
     //const [result] = await pool.query('SELECT "SI HAY CONEXION CON LA BD" AS result')
 
     try{
-        const consulta = `DROP TABLE RUNNER;
-        DROP TABLE DIGITADOR;
-        DROP TABLE ADMINISTRADOR`; 
+        const consulta = `show tables;`; 
     
      
         const [result] = await pool.query(consulta);
