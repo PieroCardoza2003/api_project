a780c4fb603dd6c84172f61a717fce43c829cb18
Piero Cardoza <82919446+PieroCardoza2003@users.noreply.github.com>
2023-05-26 04:20:05 -0500

    Update index.controller.js

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,36 +4,19 @@ export const ping = async (req, res) => {
     //const [result] = await pool.query('SELECT "SI HAY CONEXION CON LA BD" AS result')
 
     try{
-        const consulta3 = `CREATE PROCEDURE sp_cambiar_estado(
-	IN _codigo varchar(7),
-    IN _estado varchar(1),
-    IN _nivel int
+        const consulta3 = `CREATE PROCEDURE sp_lista_runner(
+	IN codigo varchar(7)
 )
 BEGIN
-	DECLARE existe int default 0;
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION
-    BEGIN
-        ROLLBACK;
-        SELECT '1' as fallo;
-    END;
-
-    START TRANSACTION;
-    Select count(*) into existe from DATOS WHERE id_user = _codigo; 
-    
-		IF _nivel = 0 AND existe = 1 THEN
-			UPDATE ADMINISTRADOR SET estado = _estado  WHERE id_admin = _codigo;
-            SELECT '0' as fallo;
-		ELSEIF _nivel = 1 AND existe = 1 THEN
-			UPDATE DIGITADOR SET estado = _estado WHERE id_digitador = _codigo;
-            SELECT '0' as fallo;
-		ELSEIF _nivel = 2 AND existe = 1 THEN
-			UPDATE RUNNER SET estado = _estado WHERE id_runner = _codigo;
-            SELECT '0' as fallo;
-		ELSE
-			ROLLBACK;
-            SELECT '1' as fallo;
-		END IF;
-    COMMIT;
+	IF LENGTH(codigo) = 7 THEN
+		SELECT * FROM RUNNER r inner join DATOS d ON r.id_runner = d.id_user where r.id_runner = codigo;
+	ELSEIF codigo = 'I' THEN
+		SELECT * FROM RUNNER r inner join DATOS d ON r.id_runner = d.id_user where r.estado = 'I';
+    ELSEIF codigo = 'A' THEN
+		SELECT * FROM RUNNER r inner join DATOS d ON r.id_runner = d.id_user where r.estado = 'A';
+	ELSE 
+		SELECT '1' fallo;
+    END IF;
 END`; 
         
     
