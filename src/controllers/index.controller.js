24e24dc9a744b849816db09b2a1211601a579dd5
Piero Cardoza <82919446+PieroCardoza2003@users.noreply.github.com>
2023-05-26 04:21:22 -0500

    Update index.controller.js

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,16 +4,16 @@ export const ping = async (req, res) => {
     //const [result] = await pool.query('SELECT "SI HAY CONEXION CON LA BD" AS result')
 
     try{
-        const consulta3 = `CREATE PROCEDURE sp_lista_runner(
+        const consulta3 = `CREATE PROCEDURE sp_lista_digitador(
 	IN codigo varchar(7)
 )
 BEGIN
 	IF LENGTH(codigo) = 7 THEN
-		SELECT * FROM RUNNER r inner join DATOS d ON r.id_runner = d.id_user where r.id_runner = codigo;
+		SELECT * FROM DIGITADOR di inner join DATOS d ON di.id_digitador = d.id_user where di.id_digitador = codigo;
 	ELSEIF codigo = 'I' THEN
-		SELECT * FROM RUNNER r inner join DATOS d ON r.id_runner = d.id_user where r.estado = 'I';
+		SELECT * FROM DIGITADOR di inner join DATOS d ON di.id_digitador = d.id_user where di.estado = 'I';
     ELSEIF codigo = 'A' THEN
-		SELECT * FROM RUNNER r inner join DATOS d ON r.id_runner = d.id_user where r.estado = 'A';
+		SELECT * FROM DIGITADOR di inner join DATOS d ON di.id_digitador = d.id_user where di.estado = 'A';
 	ELSE 
 		SELECT '1' fallo;
     END IF;
