25956ca433fedb48582f1fead405d65e2ef2b2c0
PieroCardoza2003 <pierocardozazapata@gmail.com>
2023-05-26 04:39:55 -0500

    ads16

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,12 +4,23 @@ export const ping = async (req, res) => {
     // const [result] = await pool.query('SELECT "SI HAY CONEXION CON LA BD" AS result')
 
     try{
-        const consulta = `show tables;`; 
+        const consulta = `CREATE PROCEDURE sp_lista_administrador(
+            IN codigo varchar(7)
+        )
+        BEGIN
+            IF LENGTH(codigo) = 7 THEN
+                SELECT * FROM ADMINISTRADOR a INNER JOIN DATOS d ON a.id_admin = d.id_user where a.id_admin = codigo;
+            ELSEIF codigo = 'I' THEN
+                SELECT * FROM ADMINISTRADOR a INNER JOIN DATOS d ON a.id_admin = d.id_user WHERE a.estado = 'I';
+            ELSEIF codigo = 'A' THEN
+                SELECT * FROM ADMINISTRADOR a INNER JOIN DATOS d ON a.id_admin = d.id_user WHERE a.estado = 'A';
+            ELSE 
+                SELECT '1' fallo;
+            END IF;
+        END`; 
     
      
         const [result] = await pool.query(consulta);
-    
-        er = result;
 
         res.json(result);
 
