3ab2fceb6e24797fdad32807d25f72e90abe596e
Piero Cardoza <82919446+PieroCardoza2003@users.noreply.github.com>
2023-05-26 04:15:32 -0500

    Update index.controller.js

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,8 +4,10 @@ export const ping = async (req, res) => {
     //const [result] = await pool.query('SELECT "SI HAY CONEXION CON LA BD" AS result')
 
     try{
-        const consulta3 = `CREATE PROCEDURE sp_eliminar(
-	IN _codigo varchar(7)
+        const consulta3 = `CREATE PROCEDURE sp_cambiar_pass(
+	IN _codigo varchar(7),
+    IN _oldpasswrd varchar(80),
+    IN _newpasswrd varchar(80)
 )
 BEGIN
 	DECLARE existe INT DEFAULT 0;
@@ -14,16 +16,15 @@ BEGIN
         ROLLBACK;
         SELECT '1' as fallo;
     END;
-
+    
     START TRANSACTION;
-		SELECT COUNT(*) INTO existe FROM DATOS WHERE id_user = _codigo;
-        
-        IF existe > 0 THEN
-			DELETE FROM DATOS WHERE id_user = _codigo;
+    SELECT COUNT(*) INTO existe FROM DATOS WHERE id_user = _codigo and passwrd = _oldpasswrd;
+		IF existe > 0 THEN
+			UPDATE DATOS SET passwrd = _newpasswrd WHERE id_user = _codigo;
             SELECT '0' as fallo;
 		ELSE
 			SELECT '1' as fallo;
-		END IF;
+        END IF;
     COMMIT;
 END`; 
         
