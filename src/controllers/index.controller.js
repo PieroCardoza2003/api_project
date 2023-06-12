85093c3e776e73af2fd38cdeb2ac9d75df7c947e
PieroCardoza2003 <pierocardozazapata@gmail.com>
2023-06-12 00:14:05 -0500

    time

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,8 +4,8 @@ export const ping = async (req, res) => {
     // const [result] = await pool.query('SELECT "SI HAY CONEXION CON LA BD" AS result')
 
     try{
-        const consulta = `SELECT "¡LA CONEXION CON LA BASE DE DATOS ES CORRECTA!"`; 
-    
+        //const consulta = `SELECT "¡LA CONEXION CON LA BASE DE DATOS ES CORRECTA!"`; 
+        const consulta = `SELECT CURRENT_TIME()`; 
      
         const [result] = await pool.query(consulta);
 
