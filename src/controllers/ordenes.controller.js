26a10d8e647efb7383fa1727cd3ff1b2a82708b8
PieroCardoza2003 <pierocardozazapata@gmail.com>
2023-06-20 04:16:29 -0500

    me

--- src/controllers/ordenes.controller.js
+++ src/controllers/ordenes.controller.js
@@ -185,9 +185,8 @@ export const entregaPitstop = async (req, res, next) => {
 export const ordenesEntregadas = async (req, res,) => {
     try {
         const id = req.params.id
-        const [rows] = await pool.query('CALL sp_lista_ordenes(?,?)', [3, id]);
-        res.json(rows[0])
-        return rows[0];
+        //const [rows] = await pool.query('CALL sp_lista_ordenes(?,?)', [3, id]);
+        res.json(id)
     } catch (error) {
         return 'Ocurrio un error';
     }
